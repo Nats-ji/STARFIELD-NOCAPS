7435e5fd9d3074a816e24aa1623844df4373e215
Mingming Cui <nats_ji@hotmail.com>
2023-09-08 03:27:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# STARFIELD-NOCAPS
+
+A crude script to convert all uppercase words in the UI of Starfield to capital case.
+
+Released at https://www.nexusmods.com/starfield/mods/1246/
+
+
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112916-1463291115.png)
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112889-438038306.png)
+
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112910-1267348542.png)
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112897-290197854.png)
+
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112910-2097748195.png)
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112891-234280736.png)
+
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112916-617947436.png)
+![](https://staticdelivery.nexusmods.com/mods/4187/images/1246/1246-1694112895-2005422366.png)
---
+++ src/main.js
@@ -0,0 +1,141 @@
+import * as fs from "fs";
+import * as child_process from "child_process";
+import wordlist from "wordlist-english";
+
+let englishWords = [];
+englishWords = englishWords.concat(wordlist["english"], wordlist["english/american"]);
+
+// exclude words
+{
+    let excludes = ["em"];
+    for (const word of excludes) {
+        const idx = englishWords.indexOf(word);
+        englishWords.splice(idx, 1);
+    }
+
+    // add words
+    let includes = [
+        "ONS",
+        "GAMEPLAY",
+        "QUICKSAVE",
+        "AUTOSAVE",
+        "EXITSAVE",
+        "CARBONDIOXIDE",
+        "THROWABLES",
+        "CHARGEN",
+        "DERMAESTHETIC",
+        "DIGIPICKS",
+        "STARMAP",
+        "STIMPAK",
+        "SYRINGER",
+        "UNASSIGN",
+        "UNCATEGORIZED",
+        "UNDOCK",
+        "UNEQUIP",
+        "UNSELECT",
+        "RADAWAY",
+    ];
+    includes.forEach((s, i) => (includes[i] = s.toLowerCase()));
+    englishWords = englishWords.concat(includes);
+}
+
+let file = fs.readFileSync("./data/translate_en.txt", { encoding: "utf16le" });
+let lines = file.split("\n");
+let new_lines = [];
+
+for (const line of lines) {
+    // extract the tranlated string
+    const seprator_idx = line.indexOf("\t");
+    const msgid = line.slice(0, seprator_idx);
+    const msgstr = line.slice(seprator_idx + 1);
+
+    // parse msgstr
+    let idx = 0;
+    let wasletter = false;
+    let word = "";
+    let idx_b = 0;
+    let idx_e = 0;
+    let words = [];
+    for (const char of msgstr) {
+        if (char.toLowerCase() != char.toUpperCase()) {
+            if (!wasletter) {
+                idx_b = idx;
+                word += char;
+                wasletter = true;
+            } else {
+                word += char;
+            }
+        } else {
+            if (wasletter) {
+                idx_e = idx - 1;
+                words.push({
+                    word,
+                    idx_b,
+                    idx_e,
+                });
+
+                wasletter = false;
+                word = "";
+                idx_b = 0;
+                idx_e = 0;
+            }
+        }
+
+        ++idx;
+    }
+
+    if (wasletter) {
+        idx_e = idx - 1;
+        words.push({
+            word,
+            idx_b,
+            idx_e,
+        });
+    }
+    // check if all caps
+    let all_cap = true;
+    for (const word of words) {
+        if (word.word != word.word.toUpperCase()) all_cap = false;
+    }
+
+    if (!all_cap) {
+        new_lines.push(line);
+        continue;
+    }
+
+    // got a list of words, now match word_list
+    for (let i = words.length - 1; i >= 0; i--) {
+        const word = words[i];
+
+        // ignore single letter
+        if (word.word.length == 1) words.splice(i, 1);
+
+        // ignore words not in the word_list
+        if (englishWords.indexOf(word.word.toLowerCase()) == -1) words.splice(i, 1);
+    }
+
+    // got a list of words to capitalize
+    for (const word of words) {
+        word.word = word.word[0].toUpperCase() + word.word.substring(1).toLowerCase();
+    }
+
+    // modify msgstr
+    let new_msgstr = msgstr;
+    for (const word of words) {
+        new_msgstr = new_msgstr.substring(0, word.idx_b) + word.word + new_msgstr.substring(word.idx_e + 1);
+    }
+
+    new_lines.push(msgid + "\t" + new_msgstr);
+}
+
+const output_path = "./output/Data/interface/";
+
+fs.rmSync("./output", { recursive: true, force: true });
+fs.mkdirSync(output_path, { recursive: true });
+
+fs.writeFileSync(output_path + "translate_en.txt", new_lines.join("\n"), { encoding: "utf16le" });
+fs.cpSync("./data/fontconfig_en.txt", output_path + "fontconfig_en.txt");
+
+child_process.execSync("tar -c -a -v -C ./output -f output/STARFIELD_NOCAPS.zip Data");
+
+console.log("Done!");
